5982e6d719d768bfd4a18adcbefa14c7b4aba508
Goncalo Amaral <goa.docs@gmail.com>
2017-10-19 02:22:06 +0100

    Person class now accepts string for ccExpire and converts to Date. Created Department class file

--- Person.java
+++ Person.java
@@ -1,19 +1,20 @@
 import java.util.Date;
+import java.text.*;
 
 class Person {
   String tipo;
   String name;
   int id;
   String password;
-  int department;
+  Department department;
   int phone;
   String morada;
   int cc;
   Date ccExpire;
 
   public Person(
-    String tipo, String name, int id, String password, int department,
-    int phone, String morada, int cc, Date ccExpire
+    String tipo, String name, int id, String password, Department department,
+    int phone, String morada, int cc, String ccExpire
   ) {
     this.tipo = tipo;
     this.name = name;
@@ -23,6 +24,12 @@ class Person {
     this.phone = phone;
     this.morada = morada;
     this.cc = cc;
-    this.ccExpire = ccExpire;
+
+    try {
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      this.ccExpire = dateFormat.parse(ccExpire);
+    } catch(ParseException e) {
+      System.out.println("BAD DATE FORMAT: ccExpire is not following the \"yyyy-MM-dd\" date format" );
+    }
   }
 }
