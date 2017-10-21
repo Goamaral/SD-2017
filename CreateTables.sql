2079729005ff82344e34a1432bf9ecc9e71cdb4d
Erbarbar <filipelopez3@gmail.com>
2017-10-21 20:16:51 +0100

    Fix CreateTables errors; Add CreateUser.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,9 +1,54 @@
 drop table person;
-drop table election;
-drop table list;
+drop table votingList;
 drop table department;
 drop table faculty;
- 
+drop table election;
+
+/*
+Creates table "election", which has a unique id for referencing.
+Each "election" line represents an election of a list (from a pool of lists).
+*/
+CREATE TABLE election
+(elId           int                 PRIMARY KEY,
+ elName         VARCHAR(255)        NOT NULL,
+ elDesc         VARCHAR(255),
+ elType         VARCHAR(6)          NOT NULL,
+ elStart        DATE                NOT NULL,
+ elEnd          DATE                NOT NULL  
+);
+
+/*
+Creates table "faculty", which has a unique name
+Each "faculty" line represents a group of university departments concerned with a major division of knowledge. 
+*/
+CREATE TABLE faculty
+(facName        VARCHAR(255)        PRIMARY KEY
+);
+
+/*
+Creates table "department", with a unique name
+Each "department" line represents a building of the university dealing with a specific area of activity.
+*/
+CREATE TABLE department
+(depName        VARCHAR(255)        PRIMARY KEY,
+ faculty        VARCHAR(255)        ,
+                                    FOREIGN KEY (faculty)
+                                        REFERENCES faculty(facName)
+);
+
+/*
+Creates table "votingList", which has a unique id for referencing.
+Each "votingList" line represents a group of people who want to get elected
+*/
+CREATE TABLE votingList
+(listId         int                 NOT NULL        PRIMARY KEY,
+ listName       VARCHAR(30)         NOT NULL,
+ elID           int, 
+                                    FOREIGN KEY (elID) 
+                                        REFERENCES election(elId)
+);
+
+
 /* 
 Creates table "person", which is identified by its cc number.
 Password is encrypted
@@ -12,55 +57,19 @@ Each "person" line represents one person (voter)
 CREATE TABLE person
 (type           VARCHAR(15)         NOT NULL,
  name           VARCHAR(255)        NOT NULL,
- id             int                 NOT NULL,
+ personId       int                 NOT NULL,
  password       VARCHAR(30)         NOT NULL,
- department     VARCHAR(100)        NOT NULL,
+ depName        VARCHAR(100)           NOT NULL, 
+                                    FOREIGN KEY (depName) 
+                                        REFERENCES department(depName),
  phone          int,
  address        VARCHAR(255),
  cc             int                 PRIMARY KEY,
  ccExpire       DATE,
- listName       VARCHAR(15)
+ listId         int,
+                                    FOREIGN KEY (listId)
+                                        REFERENCES votingList(listId)
 );
  
-/*
-Creates table "election", which has a unique id for referencing.
-Each "election" line represents an election of a list (from a pool of lists).
-*/
-CREATE TABLE election
-(id             int                 PRIMARY KEY,
- name           VARCHAR(255)        NOT NULL,
- description    VARCHAR(255),
- type           VARCHAR(6)          NOT NULL,
- start          DATE                NOT NULL,
- end            DATE                NOT NULL,   
-);
-
-/*
-Creates table "list", which has a unique id for referencing.
-Each "list" line represents a voting list (a group of people who want to get elected)
-*/
-CREATE TABLE list
-(id             int                 NOT NULL        PRIMARY KEY,
- name           VARCHAR(30)         NOT NULL,
- election       int                 FOREIGN KEY REFERENCES election(id)
-);
- 
-/*
-Creates table "department", with a unique name
-Each "department" line represents a building of the university dealing with a specific area of activity.
-*/
-CREATE TABLE department
-(name           VARCHAR(255)        PRIMARY KEY,
- faculty        VARCHAR(255)        FOREIGN KEY REFERENCES faculty(name)
-);
-
-/*
-Creates table "faculty", which has a unique name
-Each "faculty" line represents a group of university departments concerned with a major division of knowledge. 
-*/
-CREATE TABLE faculty
-(name           VARCHAR(255)        PRIMARY KEY,
-);
-
 
 COMMIT;
