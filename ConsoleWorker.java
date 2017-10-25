000ecc99c57b498394ba2967aae48ebc9b758ec8
Goncalo Amaral <goa.docs@gmail.com>
2017-10-25 17:05:34 +0100

    Swapped tabs for spaces ConsoleWorker

--- ConsoleWorker.java
+++ ConsoleWorker.java
@@ -27,62 +27,62 @@ class ConsoleWorker extends Thread {
                 System.out.println("WORKING ON " + job.instruction);
                 switch (job.instruction) {
                   case "Person Register Student":
-            			case "Person Register Teacher":
-            			case "Person Register Employee":
+                  case "Person Register Teacher":
+                  case "Person Register Employee":
                     this.registry.createPerson((Person)job.data1);
-            				break;
+                    break;
 
-            			case "Zone Faculty Add":
+                  case "Zone Faculty Add":
                     this.registry.createZone((Faculty)job.data1);
-            				break;
-            			case "Zone Faculty Edit":
+                    break;
+                  case "Zone Faculty Edit":
                     this.registry.updateZone((Faculty)job.data1, (Faculty)job.data2);
-            				break;
-            			case "Zone Faculty Remove":
+                    break;
+                  case "Zone Faculty Remove":
                     this.registry.removeZone((Faculty)job.data1);
-            				break;
+                    break;
 
-            			case "Zone Department Add":
+                  case "Zone Department Add":
                     this.registry.createZone((Department)job.data1);
-            				break;
-            			case "Zone Department Edit":
+                    break;
+                  case "Zone Department Edit":
                     this.registry.updateZone((Department)job.data1, (Department)job.data2);
-            				break;
-            			case "Zone Department Remove":
+                    break;
+                  case "Zone Department Remove":
                     this.registry.removeZone((Department)job.data1);
-            				break;
+                    break;
 
-            			case "Election General Student-Election Add":
-            			case "Election General Teacher-Election Add":
-            			case "Election General Employee-Election Add":
-            			case "Election Nucleus Add":
+                  case "Election General Student-Election Add":
+                  case "Election General Teacher-Election Add":
+                  case "Election General Employee-Election Add":
+                  case "Election Nucleus Add":
                     this.registry.createElection((Election)job.data1);
-            				break;
-            			case "Election General Student-Election List Candidate Add":
-            			case "Election General Teacher-Election List Candidate Add":
-            			case "Election General Employee-Election List Candidate Add":
-            			case "Election Nucleus Candidate Add":
+                    break;
+                  case "Election General Student-Election List Candidate Add":
+                  case "Election General Teacher-Election List Candidate Add":
+                  case "Election General Employee-Election List Candidate Add":
+                  case "Election Nucleus Candidate Add":
                     this.registry.addCandidate((List)job.data1, (Person)job.data2);
-            				break;
+                    break;
                   case "Election General Student-Election List Candidate Remove":
-            			case "Election General Teacher-Election List Candidate Remove":
-            			case "Election General Employee-Election List Candidate Remove":
+                  case "Election General Teacher-Election List Candidate Remove":
+                  case "Election General Employee-Election List Candidate Remove":
                   case "Election Nucleus Candidate Remove":
                     this.registry.removeCandidate((List)job.data1, (Person)job.data2);
                     break;
-            			case "Election General Student-Election VotingTable Add":
-            			case "Election General Teacher-Election VotingTable Add":
-            			case "Election General Employee-Election VotingTable Add":
-            			case "Election Nucleus VotingTable Add":
+                  case "Election General Student-Election VotingTable Add":
+                  case "Election General Teacher-Election VotingTable Add":
+                  case "Election General Employee-Election VotingTable Add":
+                  case "Election Nucleus VotingTable Add":
                     this.registry.createVotingTable((VotingTable)job.data1);
-            				break;
-            			case "Election General Student-Election VotingTable Remove":
-            			case "Election General Teacher-Election VotingTable Remove":
-            			case "Election General Employee-Election VotingTable Remove":
-            			case "Election Nucleus VotingTable Remove":
+                    break;
+                  case "Election General Student-Election VotingTable Remove":
+                  case "Election General Teacher-Election VotingTable Remove":
+                  case "Election General Employee-Election VotingTable Remove":
+                  case "Election Nucleus VotingTable Remove":
                     this.registry.removeVotingTable((VotingTable)job.data1);
                     break;
-            		}
+                }
               } catch (RemoteException e) {
                 System.out.println("WORKER FAILED " + e);
                 synchronized (jobs) {
