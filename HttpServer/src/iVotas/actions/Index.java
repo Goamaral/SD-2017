4ac38b84a7bbba9c4fb86d247ddbca5b51b52ef9
Goncalo Amaral <goa.docs@gmail.com>
2017-12-13 14:18:18 +0000

    Deploy setup

---
+++ HttpServer/src/iVotas/actions/Index.java
@@ -0,0 +1,15 @@
+package iVotas.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Index extends ActionSupport {
+
+    public String index() {
+        return SUCCESS;
+    }
+
+    // Login
+    public String login() {
+        return SUCCESS;
+    }
+}
